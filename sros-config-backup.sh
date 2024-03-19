42eea754927eb27d8ff7736879b76841e0206dcc
Giancarlo Garcia <giancarlo3g@gmail.com>
2024-03-19 18:47:19 +0000

    first commit

---
+++ sros-config-backup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+yes | cp ./clab-srmpls/R01-IXR/tftpboot/config.txt ./config/R01-IXR.cfg
+yes | cp ./clab-srmpls/R02-IXR/tftpboot/config.txt ./config/R02-IXR.cfg
+yes | cp ./clab-srmpls/R03-IXR/tftpboot/config.txt ./config/R03-IXR.cfg
+yes | cp ./clab-srmpls/R04-IXR/tftpboot/config.txt ./config/R04-IXR.cfg
+yes | cp ./clab-srmpls/R05-SR/tftpboot/config.txt ./config/R05-SR.cfg
+yes | cp ./clab-srmpls/R06-SR/tftpboot/config.txt ./config/R06-SR.cfg
+yes | cp ./clab-srmpls/R07-SR/tftpboot/config.txt ./config/R07-SR.cfg
+yes | cp ./clab-srmpls/R08-SR/tftpboot/config.txt ./config/R08-SR.cfg
+yes | cp ./clab-srmpls/R09-SR/tftpboot/config.txt ./config/R09-SR.cfg
+yes | cp ./clab-srmpls/R10-SR/tftpboot/config.txt ./config/R10-SR.cfg
+yes | cp ./clab-srmpls/R11-SR/tftpboot/config.txt ./config/R11-SR.cfg
+yes | cp ./clab-srmpls/R12-SR/tftpboot/config.txt ./config/R12-SR.cfg
+yes | cp ./clab-srmpls/R13-IXR/tftpboot/config.txt ./config/R13-IXR.cfg
+yes | cp ./clab-srmpls/R14-IXR/tftpboot/config.txt ./config/R14-IXR.cfg
+yes | cp ./clab-srmpls/R15-IXR/tftpboot/config.txt ./config/R15-IXR.cfg
+yes | cp ./clab-srmpls/R16-IXR/tftpboot/config.txt ./config/R16-IXR.cfg
